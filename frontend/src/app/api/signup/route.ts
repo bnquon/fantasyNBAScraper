9dc24338909200e46fc2f6ef15ec7f66d7740bf9
Brandon Quon <bqa7@sfu.ca>
2024-10-14 19:30:58 -0700

    Added public folder

--- frontend/src/app/api/signup/route.ts
+++ frontend/src/app/api/signup/route.ts
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
 import { NextRequest, NextResponse } from 'next/server';
 
-type dataType = {
+type Email = {
     email: string;
 }
 
 export async function POST(req: NextRequest) {
 
     const url = process.env.MONGO_URL;
-    const data: dataType = await req.json();
+    const data: Email = await req.json();
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const client = new MongoClient(url!);
 
